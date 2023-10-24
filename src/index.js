eb1807b60f35eb0c5216973f06610bd73e0620e9
arundevarajan2001 <63626641+arundevarajan2001@users.noreply.github.com>
2023-10-24 16:10:07 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import Home from "./Home";
+import Login from './Form/Login'
+import Signup from './Form/Signup'
+import DirectChatPage from "./App/App";
+import Form_index from "./Form/Form_Index";
+
+function HomePage(){
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}>
+                <Route index element={<Form_index/>} />
+                <Route path="login" element={<Login/>} />
+                <Route path="signup" element={<Signup/>} />
+            </Route>
+            <Route path="app" element={<DirectChatPage/>} />
+        </Routes>
+        </BrowserRouter>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HomePage/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
